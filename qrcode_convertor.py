25d14ce871494712e79857e05bb8bea95d26fbf3
Iliya <ilyakuzmin9@gmail.com>
2023-05-21 12:21:26 +0300

    refactor: main.py -> 13_roman_to_integer.py, delete redundant

--- qrcode_convertor.py
+++
@@ -1,9 +0,0 @@
-import qrcode
-# пример данных
-data = "https://one.okkam.group/"
-# имя конечного файла
-filename = "okkam.png"
-# генерируем qr-код
-img = qrcode.make(data)
-# сохраняем img в файл
-img.save(filename)

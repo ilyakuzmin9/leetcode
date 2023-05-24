8930acf5a388d39bd3eefd14d7e5a4466ebbdac6
Iliya <ilyakuzmin9@gmail.com>
2023-05-24 23:19:04 +0300

    add: 9_palindrome-number.py

---
+++ 9_palindrome-number.py
@@ -0,0 +1,17 @@
+def isPalindrome(x: int) -> bool:
+    # Base condition
+    if x < 0:
+        return False
+    # Store the number in a variable
+    number = x
+    # This will store the reverse of the number
+    reverse = 0
+    while number:
+        reverse = reverse * 10 + number % 10
+        number //= 10
+    return x == reverse
+
+
+if __name__ == '__main__':
+    y = 1331
+    z = isPalindrome(y)

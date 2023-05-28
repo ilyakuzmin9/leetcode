9c3e4262314f4d2a45fb3a20aed7adb198fd7482
Iliya <ilyakuzmin9@gmail.com>
2023-05-28 18:00:29 +0300

    refactor: 1_two_sum.py

--- 1_two_sum.py
+++ 1_two_sum.py
@@ -1,7 +1,32 @@
+"""
+Given an array of integers nums and an integer target,
+return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+
+Example 1:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+"""
+
 from typing import List
 import time
 import random
 
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash_t = {}
+
+        for i in range(len(nums)):
+            item = target - nums[i]
+            if item in hash_t and i != hash_t[item]:
+                return [hash_t[item], i]
+            else:
+                hash_t[nums[i]] = i
+
+        return []
 
 # def twoSum(nums: List[int], target: int) -> List[int]:
 #     list_len = range(len(nums))
@@ -16,18 +41,18 @@ import random
 #     return[]
 
 
-def twoSum(nums: List[int], target: int) -> List[int]:
-    hash_table = {}
-    # for i in range(len(nums)):
-    #     hash_table[nums[i]] = i
-    for i, val in enumerate(nums):
-        hash_table[val] = i
-
-    for j in range(len(nums)):
-        item = target - nums[j]
-        if item in hash_table and j != hash_table[item]:
-            return [j, hash_table[item]]
-    return []
+# def twoSum(nums: List[int], target: int) -> List[int]:
+#     hash_table = {}
+#     # for i in range(len(nums)):
+#     #     hash_table[nums[i]] = i
+#     for i, val in enumerate(nums):
+#         hash_table[val] = i
+#
+#     for j in range(len(nums)):
+#         item = target - nums[j]
+#         if item in hash_table and j != hash_table[item]:
+#             return [j, hash_table[item]]
+#     return []
 
 
 # def twoSum(nums: List[int], target: int) -> List[int]:
@@ -41,10 +66,13 @@ def twoSum(nums: List[int], target: int) -> List[int]:
 
 if __name__ == '__main__':
     # Generate 5 random numbers between 10 and 30
-    random_list = random.sample(range(1, 100), 99)
-    # nums = [1, 2, 9, 9, 4]
-    target = 1555
+    # r_list = random.sample(range(1, 100), 99)
+    r_list = [3,3]
+    target = 6
+    sol = Solution()
     start = time.time()
-    print(twoSum(random_list, target))
+    res = sol.twoSum(r_list, target)
+
+    print(res)
     end = time.time()
     print(end - start)

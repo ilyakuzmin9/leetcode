ee8651e0245732888e3b54c9c7ad554624a58ac1
Iliya <ilyakuzmin9@gmail.com>
2023-05-29 10:41:15 +0300

    add: solved 1547_min_cost_cut_stick.py

--- 1547_min_cost_cut_stick.py
+++ 1547_min_cost_cut_stick.py
@@ -15,7 +15,22 @@ from typing import List
 class Solution:
     def minCost(self, n: int, cuts: List[int]) -> int:
 
-        return 1
+        hash_map = {}
+
+        def dfs(left, right):
+            if right - left == 1:
+                return 0
+            if (left, right) in hash_map:
+                return hash_map[(left, right)]
+
+            result = float("inf")
+            for cut in cuts:
+                if left < cut < right:
+                    result = min(result, (right - left) + dfs(left, cut) + dfs(cut, right))
+            hash_map[(left, right)] = result = 0 if result == float("inf") else result
+            return result
+
+        return dfs(0, n)
 
 
 if __name__ == '__main__':

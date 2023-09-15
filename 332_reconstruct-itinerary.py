8ec9c702e9fbe3e0a792e69f547a5bf62a80ca8e
Iliya <ilyakuzmin9@gmail.com>
2023-09-15 20:23:12 +0300

    add: 332_reconstruct-itinerary.py

---
+++ 332_reconstruct-itinerary.py
@@ -0,0 +1,51 @@
+"""
+You are given a list of airline tickets where tickets[i] = [fromi, toi] represent the departure and the arrival airports of one flight. Reconstruct the itinerary in order and return it.
+
+All of the tickets belong to a man who departs from "JFK", thus, the itinerary must begin with "JFK". If there are multiple valid itineraries, you should return the itinerary that has the smallest lexical order when read as a single string.
+
+For example, the itinerary ["JFK", "LGA"] has a smaller lexical order than ["JFK", "LGB"].
+You may assume all tickets form at least one valid itinerary. You must use all the tickets once and only once.
+
+
+"""
+from typing import List
+
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        adj = {src: [] for src, dst in tickets}
+
+        tickets.sort()
+        for src, dst in tickets:
+            adj[src].append(dst)
+
+        res = ['JFK']
+        def dfs(src):
+            if len(res) == len(tickets) + 1:
+                return True
+
+            if src not in adj:
+                return False
+
+            temp = list(adj[src])
+            for i, v in enumerate(temp):
+                adj[src].pop(i)
+                res.append(v)
+
+                if dfs(v):
+                    return True
+
+                adj[src].insert(i, v)
+                res.pop()
+
+            return False
+
+        dfs('JFK')
+        return res
+
+
+if __name__ == '__main__':
+    tickets = [["JFK", "SFO"], ["JFK", "ATL"], ["SFO", "ATL"], ["ATL", "JFK"], ["ATL", "SFO"]]
+    sol = Solution()
+    result = sol.findItinerary(tickets)
+    print(result)
+
